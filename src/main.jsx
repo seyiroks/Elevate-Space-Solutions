86c60d7aac4c8cb232fa60dd5bcb2e59ba820b30
Andy Iroko <andy.iroko.si@gmail.com>
2026-05-06 16:31:49 +0000

    self-host Montserrat font to reduce render-blocking requests

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import "@fontsource/montserrat/400.css";
+import "@fontsource/montserrat/500.css";
+import "@fontsource/montserrat/600.css";
+import "@fontsource/montserrat/700.css";
+import "@fontsource/montserrat/800.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
